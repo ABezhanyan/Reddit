503e0073c2e42a01787d0250e20abef2512f3161
hipster24930@gmail.com <Agasik19>
2022-02-27 18:43:22 +0300

    REDDIT API :) FIX

--- src/App.js
+++ src/App.js
@@ -17,30 +17,32 @@ function App() {
         req()
     }, [])
 
-    const onClick = async () => {{
-            const tata = await fetch(`https://www.reddit.com/r/cats.json?after=${after}`)
-            const tutu = await tata.json()
-            setCats([...cats,...tutu.data.children])
-            setAfter(tutu.data.after)
+    const onClick = async () => {
+        const tata = await fetch(`https://www.reddit.com/r/cats.json?after=${after}`)
+        const tutu = await tata.json()
+        setCats([...cats,...tutu.data.children])
+        setAfter(tutu.data.after)
 
     }
     return (
         <div className="main">
-            <div>
+            <div className="content_main">
                 {cats && cats.map(cat => (
-                    <div>
-                        <span> {cat.data.title}</span>
-                        {cat.data.url_overridden_by_dest && (<img width="800px" height="800px" src={cat.data.url_overridden_by_dest}/>)}
-                        {cat.data.secure_media?.reddit_video &&
-                            <video width="800px" height="800px" controls >
-                                <source src={cat.data.secure_media.reddit_video.fallback_url} type="video/mp4"></source>
-                            </video>}
+                    <div className="content">
+                        {
+                            cat.data.secure_media?.reddit_video ? (
+                                <video autoplay controls >
+                                    <source src={cat.data.secure_media.reddit_video.fallback_url} type="video/mp4"></source>
+                                </video>) : cat.data.url_overridden_by_dest && (<img src={cat.data.url_overridden_by_dest}/>)
+                        }
                     </div>
                 ))}
+                <button className="btn" onClick={onClick}>ЕЩЕ КОТИКОВ</button>
             </div>
-            <button className="btn" onClick={onClick}>ЕЩЕ КОТИКОВ</button>
+
         </div>
     );
 }
 
 export default App;
+
